0f36804a6b1d6d6f74cc01fd05a1a89e39be3050
gaheunjem <gaheunjem@sookmyung.ac.kr>
2019-11-10 22:40:17 +0900

    11/10

---
+++ card.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <time.h> 
+#include <stdlib.h>
+
+
+ 
+
+
+
+void CardTray()
+{
+	
+	
+	int cardnum[]={'A','2','3','4','5','6','7','8','9','10','J','Q','K',
+	'A','2','3','4','5','6','7','8','9','10','J','Q','K',
+	'A','2','3','4','5','6','7','8','9','10','J','Q','K',
+	'A','2','3','4','5','6','7','8','9','10','J','Q','K'};
+	int cardshape[] = {'H','S','C','D','H','S','C','D','H','S','C','D','H','S','C','D'
+	,'H','S','C','D','H','S','C','D','H','S','C','D','H','S','C','D','H','S','C','D'
+	,'H','S','C','D','H','S','C','D','H','S','C','D','H','S','C','D'};
+	
+	srand(time(NULL));
+	
+	int temp1;
+	int rn1;
+	int i;
+	for (i=0;i<52;i++)
+	{
+		rn1 = rand()% 52;
+		temp1 = cardnum[i];
+		cardnum[i] = cardnum[rn1];
+		cardnum[rn1] = temp1;
+	}
+	
+	int temp2;
+	int rn2;
+	int j;
+	
+	for (j=0;j<52;j++)
+	{
+		rn2 = rand()% 52;
+		temp2 = cardshape[j];
+		cardshape[j] = cardshape[rn2];
+		cardshape[rn2] = temp2;
+	}
+	
+	printf ("%c%c",cardshape[1],cardnum[1]);
+	
+	
+	
+	
+}
---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	int start;
+	int numpy;
+	printf("######blackjak######\n");
+	printf("enter 1 to start game\n");
+	scanf("%d",&start);
+	if(start==1)
+	{
+		printf("enter number of player(1~5): ");
+	}
+	
+ 	
+	
+	CardTray();
+	
+	
+	
+	
+	
+	return 0;
+}
+
+
+	
+
